424ece842a78bd0d221348c1345dc6aed1190cc7
Md. Shariful Islam <102409828+Shariful797@users.noreply.github.com>
2025-03-31 15:31:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -507,7 +507,7 @@ def main(disable_exit=False):
                     l_name, l_key, l_out_date = EK_obj.getLD()
                     pc_use = r"*💻 Pc or Laptop*: Use the key without connecting your account\!"
                     important_note = ">❤️Show Your Support with Reactions❤️"
-                    photo_path = 'img/essp.jpg'
+                    #photo_path = 'img/essp.jpg'
                     stay_tuned = "Stay tuned for more\!"
                     mention = "@FreeLicense1"
                     output_line = '\n'.join([
@@ -530,10 +530,9 @@ def main(disable_exit=False):
                     print(output_line)
                     full_message = f"{output_lines}\n{pc_use}\n\n\n{important_note}\n\n\n{stay_tuned}\n{mention}\n"
                     try:
-                        with open(photo_path, 'rb') as photo:
-                            bot.send_photo(chat_id=-1002671083215, photo=photo, caption=full_message, parse_mode='MARKDOWNv2')
+                        bot.send_message(chat_id=-1002671083215, text=full_messages, parse_mode='MARKDOWNv2')
                     except Exception as e:
-                        print(f"Error sending photo: {e}")
+                        print(f"Error sending message: {e}")
                     if args['vpn_codes']:
                         EV_obj = EV(email_obj, driver, ER_obj.window_handle)
                         EV_obj.sendRequestForVPNCodes()
@@ -543,7 +542,7 @@ def main(disable_exit=False):
                             vpn_sub_title = "Over 60 locations to choose"
                             proof = "📸Share a screenshot in the comments\!"
                             important_note = ">❤️Show Your Support with Reactions❤️"
-                            photo_path = 'img/ESET_VPN.png'
+                            #photo_path = 'img/ESET_VPN.png'
                             stay_tuned = "Stay tuned for more\!"
                             mention = "@@reeLicense1"
                             vpn_codes_line = '\n\n'.join([f'Key: `{vpn_code}`' for vpn_code in vpn_codes])
@@ -570,10 +569,9 @@ def main(disable_exit=False):
                             print(output_line)
                             full_messages = f"{vpn_title}\n{vpn_sub_title}\n\n\n{vpnoutput_line}\n\n\n{important_note}\n\n\n{proof}\n\n\n{stay_tuned}\n{mention}\n"
                             try:
-                                with open(photo_path, 'rb') as photo:
-                                    bot.send_photo(chat_id=-1002671083215, photo=photo, caption=full_messages, parse_mode='MARKDOWNv2')
+                                bot.send_message(chat_id=-1002671083215, text=full_messages, parse_mode='MARKDOWNv2')
                             except Exception as e:
-                                print(f"Error sending photo: {e}")
+                                print(f"Error sending message: {e}")
 
             # ESET ProtectHub
             elif args['protecthub_account'] or args['advanced_key']:
